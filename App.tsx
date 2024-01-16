ad0a6fdf40df8a8cc5bc833c4436f1caa1275352
grr1203 <gyrms9412@naver.com>
2024-01-16 14:36:08 +0900

    Splash Screen 구현

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useEffect} from 'react';
 import HomeScreen from './views/HomeScreen';
 import LoginScreen from './views/LoginScreen';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+  });
+
   return (
     <NavigationContainer>
       <Stack.Navigator>

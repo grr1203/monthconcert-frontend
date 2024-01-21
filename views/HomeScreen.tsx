7a6f690ada3ba4d05f909bd61d73230ef49892b8
grr1203 <gyrms9412@naver.com>
2024-01-21 17:52:53 +0900

    자동 로그인 체크 로직 구현

--- views/HomeScreen.tsx
+++ views/HomeScreen.tsx
@@ -3,13 +3,32 @@ import {SafeAreaView, ScrollView, StatusBar} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {
+  getJWTHeaderFromLocalStorage,
+  privateAxiosInstance,
+} from '../services/axios.service';
 
 function HomeScreen({
   navigation,
 }: NativeStackScreenProps<any>): React.JSX.Element {
   useEffect(() => {
-    navigation.navigate('Login');
-  });
+    (async function checkLogin() {
+      // calendar api 호출 (check login token)
+      // todo: api 변경
+      try {
+        const res = await privateAxiosInstance.get('/user', {
+          params: {},
+          headers: await getJWTHeaderFromLocalStorage(),
+        });
+        console.log('[res data]', res.data);
+      } catch (error) {
+        // api 호출 실패 -> access token 갱신 실패 -> 로그인 화면으로 이동
+        console.log('[error]', error);
+        navigation.navigate('Login');
+      }
+    })();
+  }, [navigation]);
+
   return (
     <SafeAreaView style={{backgroundColor: Colors.lighter}}>
       <StatusBar

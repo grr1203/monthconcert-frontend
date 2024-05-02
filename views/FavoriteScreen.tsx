cac2871c54c0849c6c3821aef9fa4aa661c2798b
grr1203 <gyrms9412@naver.com>
2024-05-02 16:35:09 +0900

    css 업데이트

--- views/FavoriteScreen.tsx
+++ views/FavoriteScreen.tsx
@@ -268,7 +268,7 @@ const ArtistRow = ({item}: {item: any}) => {
     <View>
       <View
         key={item.instagram_account}
-        className="flex-row item-center justify-between px-6 py-2">
+        className="flex-row item-center justify-between px-6 py-2 bg-white">
         <Text className="text-base font-bold text-[#555]">
           {item.artist_name}
         </Text>
